4722be2e3b61269fcd0026e44a50ed879b62ea28
Adam Farley <farleyadamc@gmail.com>
2024-10-27 18:46:53 -0500

    part 4 is working, success!

--- R-ALAB-308-4-1-Solution.js
+++ R-ALAB-308-4-1-Solution.js
@@ -37,4 +37,29 @@ while (rowdataIndex < rowdata.length) {
     arraydata.push(obj); // Adds the object to the arraydata array
     rowdataIndex++;
 }
+//console.log(arraydata);
+
+//================================Part 4: Sorting and Manipulating Data =====================================
+
+const removeEnd = arraydata.pop();
 console.log(arraydata);
+
+const insertIndex = { id: "48", name: "Barry", occupation: "Runner", age: "25" };
+arraydata.splice(1, 0, insertIndex); //Remove the last element from the sorted array and insert the following object at index 1
+
+const insertLast = { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+arraydata.push(insertLast) //Add the following object to the end of the array
+console.log(arraydata); //Prints out the array data to verify above statements worked correctly and they did :)
+
+let totalAges = 0;
+let ageCounter = 0;
+for (let i = 0; i < arraydata.length; i++) {
+    totalAges += Number(arraydata[i].age); // Converts the string into a number and adds the value to totalAges
+    ageCounter++;
+}
+console.log(totalAges);
+console.log(totalAges/ageCounter);
+//This is the average age
+let avg = totalAges/ageCounter;
+//I decided to round up as that what you normally do in math when a number is above 5, I prefer whole numbers instead of decimals
+console.log(Math.floor(avg));
