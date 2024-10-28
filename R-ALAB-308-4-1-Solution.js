dd1c069e01520728150f2c5e035b7220ac6729be
Adam Farley <farleyadamc@gmail.com>
2024-10-27 19:44:20 -0500

    added Parts 1-3 notation at top of page, final product

--- R-ALAB-308-4-1-Solution.js
+++ R-ALAB-308-4-1-Solution.js
@@ -2,7 +2,7 @@
 
 
 
-console.log("================================================");
+console.log("========================Parts 1-3========================");
 
 let datainfo = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
